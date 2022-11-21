4d1b438eff6a353e3547bd8e33f910d491051cd7
artem.korotenko <artem.korotenko@betterme.world>
2022-11-22 00:31:48 +0200

    updated congratulations text

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from itertools import count
+
 import words_fetcher
 import random
 
@@ -5,11 +7,25 @@ import random
 def congratulate_user():
     print(f"Congratulations, you won! your words: {guesses}")
 
+def double (word):
+    if word in guesses:
+        print("You cannot use the same word twice")
+        return True
+    return False
 
 def is_game_over():
     return guessed == WORDS_TO_WIN or errors == ERRORS_TO_LOSE
 
-
+def guess_is_valid(candidate):
+    for letter in candidate:
+        if letter not in word:
+            print(f"You can not use this letter{letter}")
+            return False
+        count = word.count(letter)
+        if count < candidate.count(letter):
+            print(f"You can use letter {letter} only {count} times")
+            return False
+    return True
 guessed = 0
 errors = 0
 
@@ -28,6 +44,9 @@ print(f"Your word is '{word}'")
 
 while not is_game_over():
     guess = input("Your next take: ")
+    if double(guess):
+        print("You cannot use the same word twice")
+        continue
     if guess in full_list:
         guessed += 1
         guesses.append(guess)
